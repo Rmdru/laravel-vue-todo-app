b1f7f1cc7cf4287a69b90c9e5387368f80b7b6f3
rmdru <rmdruijter@gmail.com>
2023-01-24 13:02:11 +0100

    feature: controllers for update and delete added

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 //create routes
-Route::get('/', [ItemController::class, 'index']);
+Route::get('/items', [ItemController::class, 'index']);
 Route::prefix('/item')->group(function() {
         Route::post('/store', [ItemController::class, 'store']);
         Route::put('/{id}', [ItemController::class, 'update']);
