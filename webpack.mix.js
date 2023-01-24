9c703bae8ed905c7f12b6f2cffed7ec3c2486ea1
rmdru <rmdruijter@gmail.com>
2023-01-24 14:37:19 +0100

    bugfix: laravel mix js url fix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,9 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
+mix.setResourceRoot("../")
+
 mix.js('resources/js/app.js', 'public/js').vue()
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
-
-mix.setResourceRoot("../")
