7e8f33507b69049ac07ca5661ed5bb033f1146bf
rmdru <rmdruijter@gmail.com>
2023-01-31 17:20:39 +0100

    feature: fixed laravel mix css http mixed content error

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://laravel-vue-todo-app-rmdru.vercel.app/'),
 
     'asset_url' => env('ASSET_URL', null),
 
