7fc4cc7efadb42cf1010d31f051b780866dc0a01
rmdru <rmdruijter@gmail.com>
2023-02-16 20:56:04 +0100

    BUGFIX: change css link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
         <!--load tailwindcss-->
-        <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}" type="text/css" />
     </head>
     <body>
         <div id="app">
