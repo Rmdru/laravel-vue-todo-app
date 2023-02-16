d5a9908f874b5f751f12b3aa8752e32b71ecb897
rmdru <rmdruijter@gmail.com>
2023-02-16 20:49:12 +0100

    BUGFIX: asset url changed

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
 
     'url' => env('APP_URL', 'https://laravel-vue-todo-app-rmdru.vercel.app/'),
 
-    'asset_url' => env('ASSET_URL', null),
+    'asset_url' => env('ASSET_URL', 'https://laravel-vue-todo-app-rmdru.vercel.app/'),
 
     /*
     |--------------------------------------------------------------------------
